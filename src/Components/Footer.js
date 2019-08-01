df2a799a49800612f6f814876dc030d03dae4f34
Brannon Wadforth <whiskey@digitalnode.io>
2019-08-01 22:07:09 +1000

    Playing around with footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,30 +1,47 @@
 import React from "react";
-import Icon from "./Util/Icon";
-import IFacebook from "../Assets/Icons/facebook.png";
-import IInstagram from "../Assets/Icons/instagram.png";
 import Logo from "../Assets/Vectors/logo.png";
 import Row from "./Util/Row";
 import Tile from "./Util/Tile";
+import { ReactComponent as Facebook } from "../Assets/Icons/facebook.svg";
+import { ReactComponent as Instagram } from "../Assets/Icons/instagram.svg";
 
 export default function Footer(props) {
   return (
     <div className="footer">
+      <div className="social-line">
+        <div className="social-icons">
+          <a href="https://www.facebook.com/FightClubTownsville/">
+            <Facebook />
+          </a>
+          <a href="https://www.instagram.com/fightclubtownsville/">
+            <Instagram />
+          </a>
+        </div>
+      </div>
       <footer>
         <Row>
           <Tile>
             <img src={Logo} alt="Fight Club Jiu-Jitsu" />
-            <br />
-            <strong>Fight Club Jiu-Jitsu | Townsville 2019 ©</strong>
           </Tile>
-          <Tile title="Details">
+          <Tile>
             <p>
+              <strong>Email:</strong>
               <a href="mailto: fightclubtsv@hotmail.com">
                 fightclubtsv@hotmail.com
               </a>
             </p>
-            <p>0484 062 027</p>
-            <p>50 Bamford Lane, Townsville - QLD, Australia</p>
-            <p>Abn: 85 937 401 694</p>
+            <p>
+              <strong>Phone:</strong> 0484 062 027
+            </p>
+            <p>
+              <strong>Address:</strong> 50 Bamford Lane, Townsville - QLD,
+              Australia
+            </p>
+            <p>
+              <strong>Abn:</strong> 85 937 401 694
+            </p>
+            <br />
+            <strong>Fight Club Jiu-Jitsu | Townsville 2019 ©</strong>
           </Tile>
         </Row>
       </footer>
