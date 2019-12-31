9c974c07acfcbe79b61196312bb8248944048edb
Brannon Wadforth <whiskey@digitalnode.io>
2019-12-31 12:46:20 +1100

    Updating BIO

--- src/Components/Instructors/InstructorList.js
+++ src/Components/Instructors/InstructorList.js
@@ -12,12 +12,14 @@ export default function InstructorList(props) {
             <Row>
                 <Tile>
                     <Instructor name="Eric Lemoine" image={EricPhoto}
-                                bio="LOREM IPSUM LOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUM" classes="Adults/Kids"/>
-                </Tile>
-
-                <Tile>
-                    <Instructor name="Eamon Mcneil" image={EricPhoto}
-                                bio="LOREM IPSUM LOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUM" classes="Kids"/>
+                                bio="Eric is a 4th stripe purple belt under the guidance of Prof Daniel Lima.
+                                Eric has over 20 yrs martial arts experience and is specialised in performance,
+                                self-defence and competition training for adults and kids.
+                                Eric has taught Military self-defence to the Australian Army including the Elite Special Forces SASR and gyms such as the prestigious SASR gym The Mill.
+                                Eric started his BJJ journey with Australia’s first BJJ black belt Prof Paulo Guimaraes back in 2000, during this time Eric has deployed
+                                on multiple overseas operations with the army.  Eric discharged from the army in 2013 and has pursued his dream of owning his own gym under the
+                                Fight Club banner. Eric holds great value in what he has learnt from Prof Paulo Guimaraes and now Prof Daniel Lima and is passionate in passing his knowledge on to his students."
+                                classes="Adults/Kids"/>
                 </Tile>
             </Row>
         </div>
